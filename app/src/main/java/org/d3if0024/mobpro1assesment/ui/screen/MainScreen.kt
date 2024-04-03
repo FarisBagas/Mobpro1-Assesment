df9c85464c40916f0ee87b8f43a9db1352dc2a21
Faris Bagas Pratama <Neocrmencer@gmail.com>
2024-04-03 13:16:50 +0700

    Membuat tampilan textfield, radiobutton, dan button (hanya tampilan saja)

--- app/src/main/java/org/d3if0024/mobpro1assesment/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if0024/mobpro1assesment/ui/screen/MainScreen.kt
@@ -1,21 +1,41 @@
 package org.d3if0024.mobpro1assesment.ui.screen
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -53,17 +73,91 @@ fun MainScreen(navController: NavHostController) {
         ScreenContent(Modifier.padding(padding))
     }
 
+
 }
 
 @Composable
 fun ScreenContent(modifier: Modifier){
+
+    var rupiah by rememberSaveable { mutableStateOf("") }
+    val radioOption = listOf(
+    stringResource(id = R.string.usd ),
+    stringResource(id = R.string.yen)
+    )
+    var mataUang by rememberSaveable { mutableStateOf(radioOption[0]) }
+
     Column (
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+
     ){
 
+        Text(
+            text = stringResource(id = R.string.info_aplikasi)
+        )
+
+    OutlinedTextField(
+        value =rupiah ,
+        onValueChange = { rupiah = it},
+        label = { Text(text = stringResource(id = R.string.mata_uang))},
+        trailingIcon = { Text(text = "Rp")},
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Number,
+            imeAction = ImeAction.Next
+        ),
+        modifier = Modifier.fillMaxWidth()
+    )
+
+    Row (
+        modifier = Modifier
+            .padding(top = 9.dp)
+            .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
+    ) {
+        radioOption.forEach{text ->
+            MataUangOption(
+                label = text,
+                isSelected = mataUang == text,
+                modifier = Modifier
+                    .selectable(
+                        selected = mataUang == text,
+                        onClick = { mataUang = text },
+                        role = Role.RadioButton
+                    )
+                    .weight(1f)
+                    .padding(16.dp)
+            )
+
+        }
     }
+     Button(
+         onClick = {
+                   /*TODO*/
+                   },
+         modifier = Modifier.padding(top = 8.dp),
+         contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+     ) {
+        Text(text = stringResource(id = R.string.hitung_konversi))
+     }
+
+    }
+}
+
+@Composable
+fun MataUangOption (label:String, isSelected: Boolean, modifier: Modifier) {
+Row (
+    modifier = modifier,
+    verticalAlignment = Alignment.CenterVertically
+){
+    RadioButton(selected = isSelected, onClick = null)
+    Text(
+        text = label,
+        style = MaterialTheme.typography.bodyLarge,
+        modifier = Modifier.padding(start = 8.dp)
+    )
+}
 }
 
 @Preview(showBackground = true)
