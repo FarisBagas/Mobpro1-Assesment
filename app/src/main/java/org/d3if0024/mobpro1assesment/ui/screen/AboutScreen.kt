372405f1db85f4fc2135db3a62d625747bd3cc1a
Faris Bagas Pratama <Neocrmencer@gmail.com>
2024-04-03 11:01:24 +0700

    membuat navigasi up untuk kembali

--- app/src/main/java/org/d3if0024/mobpro1assesment/ui/screen/AboutScreen.kt
+++ app/src/main/java/org/d3if0024/mobpro1assesment/ui/screen/AboutScreen.kt
@@ -2,7 +2,11 @@ package org.d3if0024.mobpro1assesment.ui.screen
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -12,15 +16,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import org.d3if0024.mobpro1assesment.R
 import org.d3if0024.mobpro1assesment.ui.theme.Mobpro1AssesmentTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AboutScreen() {
+fun AboutScreen(navController: NavHostController) {
     Scaffold (
         topBar = {
-            TopAppBar(title = {
+            TopAppBar(navigationIcon = {
+               IconButton(onClick = {navController.popBackStack()}) {
+                   Icon(
+                       imageVector =Icons.Filled.ArrowBack ,
+                       contentDescription = stringResource(id = R.string.kembali),
+                       tint = MaterialTheme.colorScheme.primary
+                   )
+               }
+            },
+                title = {
                 Text(text = (stringResource(id = R.string.tentang_aplikasi)))
             },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
@@ -40,6 +55,6 @@ fun AboutScreen() {
 @Composable
 fun AboutScreenPreview() {
     Mobpro1AssesmentTheme {
-        AboutScreen()
+        AboutScreen(rememberNavController())
     }
 }
